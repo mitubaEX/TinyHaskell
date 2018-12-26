910b05f74c0e2453bd95ff144e14eb1a70a8b46b
mitubaEX <g1344955@cse.kyoto-su.ac.jp>
2018-12-27 02:33:50 +0900

    add arithmetic

--- vm/eval.hs
+++ vm/eval.hs
@@ -10,9 +10,15 @@ myEval :: Cell -> Op -> String
 myEval (Node a b) _
     | a == "=" = myEval b "="
     | a == "+" = myEval b a
+    | a == "-" = myEval b a
+    | a == "/" = myEval b a
+    | a == "%" = myEval b a
 myEval (Cons a b) c
     | c == "=" = emptyMyEval a ++ "=" ++ emptyMyEval b
-    | c == "+" = emptyMyEval a ++ "+" ++ emptyMyEval b
+    | c == "+" = show $ (read $ emptyMyEval a :: Integer) + (read $ emptyMyEval b :: Integer)
+    | c == "-" = show $ (read $ emptyMyEval a :: Integer) - (read $ emptyMyEval b :: Integer)
+    | c == "/" = show $ (read $ emptyMyEval a :: Integer) `div` (read $ emptyMyEval b :: Integer)
+    | c == "%" = show $ (read $ emptyMyEval a :: Integer) `mod` (read $ emptyMyEval b :: Integer)
     | otherwise = emptyMyEval a ++ emptyMyEval b
     where emptyMyEval a = myEval a ""
 myEval (Leaf a b) _ = b
