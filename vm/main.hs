82d40235942150cb77378a9327421a5d32a9cde4
mitubaEX <g1344955@cse.kyoto-su.ac.jp>
2018-12-27 22:29:33 +0900

    add parse mult args

--- vm/main.hs
+++ vm/main.hs
@@ -9,12 +9,12 @@ myTraverse :: [String] -> Cell
 myTraverse (x:y:xs)
   | "node" `isPrefixOf` x = Node (secondElm $ splitedByRParen x) (myTraverse $ y : xs)
   | "cons" `isPrefixOf` x = Cons (myTraverse [y]) (myTraverse xs)
-  | "leaf" `isPrefixOf` x = Leaf (secondElm $ splitedByRParen $ head $ splitedBySpace x) (unpack $ dropWhileEnd (==')') $ pack . secondElm $ splitedBySpace x)
+  | "leaf" `isPrefixOf` x = Leaf (secondElm . splitedByRParen . head $ splitedBySpace x) (unpack $ dropWhileEnd (==')') $ pack . unwords . tail $ splitedBySpace x)
   where splitedBySpace = splitOn " "
         splitedByRParen = splitOn "("
         secondElm = head . tail
 myTraverse [x]
-  | "leaf" `isPrefixOf` x = Leaf (head . tail $ splitOn "(" $ head $ splitOn " " x) (unpack $ dropWhileEnd (==')') $ pack . head . tail $ splitOn " " x)
+  | "leaf" `isPrefixOf` x = Leaf (head . tail $ splitOn "(" $ head $ splitOn " " x) (unpack $ dropWhileEnd (==')') $ pack . unwords . tail $ splitOn " " x)
 myTraverse _      =  Empty
 
 main = do
