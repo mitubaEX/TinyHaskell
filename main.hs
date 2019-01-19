5ed83897c36353d6ca43fe23784163a1a3b3d85d
mitubaEX <g1344955@cse.kyoto-su.ac.jp>
2019-01-19 16:55:42 +0900

    add repl

--- main.hs
+++ main.hs
@@ -1,5 +1,6 @@
 import           Calls
 import           Cell
+import           Data.IORef
 import           Data.List          (isPrefixOf)
 import           Data.List.Split    (splitOn)
 import qualified Data.Map           as Map
@@ -10,14 +11,25 @@ import           Parser
 import           Printer
 import           Run
 import           System.Environment (getArgs)
+import           System.IO
+import           System.IO.Unsafe
 
-main = do
-  args <- getArgs
-  fileBody <- if length args < 1
-    then getContents
-    else readFile $ head args
+{-# NOINLINE funcs #-}
+funcs :: IORef FunctionMap
+funcs = unsafePerformIO $ newIORef Map.empty
 
+runRepl :: IO ()
+runRepl = do
+  putStr "REPL >>> "
+  hFlush stdout
+  fileBody <- getLine
+  runOnce fileBody
+  runRepl
+
+runOnce :: String -> IO ()
+runOnce fileBody = do
   let contents = splitOn "\n" fileBody
+
   let valList = map readExpr $ filter (not . null) contents
   -- print valList
 
@@ -26,12 +38,22 @@ main = do
 
   let functionMap = list2Map functions
   -- print functionMap
+  atomicModifyIORef funcs (\m -> (Map.union functionMap m, ()))
+  fs <- readIORef funcs
 
   let calls = callsList valList
   -- print calls
 
-  let result = performRun functionMap calls
+  let result = performRun fs calls
   -- print result
 
-  putStrLn "result:"
-  mapM performPrint result
+  mapM_ performPrint result
+
+
+main = do
+  args <- getArgs
+  if length args < 1
+    then runRepl
+    else do
+      fileBody <- readFile $ head args
+      runOnce fileBody
